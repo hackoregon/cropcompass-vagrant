f714dc390a17fd9df0fafffd54cac0245affbd50
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-05-19 22:38:27 -0700

    provision via bash script, not puppet

--- scripts/puppet_apply.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /vagrant/puppet
-
-sudo /opt/puppetlabs/bin/puppet apply ./manifests/common.pp --modulepath=./modules
-sudo /opt/puppetlabs/bin/puppet apply ./manifests/nginx.pp --modulepath=./modules
-sudo /opt/puppetlabs/bin/puppet apply ./manifests/mysql.pp --modulepath=./modules
-sudo /opt/puppetlabs/bin/puppet apply ./manifests/urbandev_frontend.pp --modulepath=./modules
-sudo /opt/puppetlabs/bin/puppet apply ./manifests/urbandev_backend.pp --modulepath=./modules
