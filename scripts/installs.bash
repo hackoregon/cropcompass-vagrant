25b47e0ad8cc926776fa1ee5245dfc2cd9696869
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-05-23 01:13:10 -0700

    use --quiet on the wget

--- scripts/installs.bash
+++ scripts/installs.bash
@@ -47,7 +47,7 @@ createdb vagrant
 
 echo "Restoring SQL dump"
 createuser alex
-wget https://www.dropbox.com/s/t6t1r3x6a4lp0zi/agtechdump2016-05-19.sql
+wget --quiet https://www.dropbox.com/s/t6t1r3x6a4lp0zi/agtechdump2016-05-19.sql
 psql < /vagrant/agtech*sql > ~/logs/psql
 
 echo "Creating 'cropcompass' virtualenv for Django"
