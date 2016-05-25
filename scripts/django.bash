75d40a40d72d7e173f212eae4607c60233de1392
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-05-24 22:33:07 -0700

    static collection done here

--- scripts/django.bash
+++ scripts/django.bash
@@ -8,7 +8,13 @@ source ~/Env/cropcompass/bin/activate
 pip install --upgrade pip > ~/logs/pip 2>&1
 pip install -r /vagrant/scripts/requirements.txt > ~/logs/requirements 2>&1
 
+echo `date` "Initializing cropcompass project"
+cp -rp /vagrant/cropcompass ~
+
+pushd ~/cropcompass
 echo `date` "Running migrations"
-pushd /vagrant/cropcompass
 python3 manage.py migrate > ~/logs/migrate 2>&1
+echo `date` "Collecting static assets"
+echo "STATIC_ROOT = os.path.join(BASE_DIR, \"static/\")" >> cropcompass/settings.py
+python3 manage.py collectstatic --noinput > ~/logs/static 2>&1
 popd
