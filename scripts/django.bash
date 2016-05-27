902db3afd5c3208c58d43e3b31d924d97d3f3344
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-05-27 14:22:23 -0700

    more informative message for virtualenv

--- scripts/django.bash
+++ scripts/django.bash
@@ -2,17 +2,16 @@
 
 echo `date` "Cloning Django application code from GitHub to '~vagrant'"
 pushd ~vagrant
-git clone https://github.com/hackoregon/cropcompass-django.git \
+git clone https://github.com/hackoregon/cropcompass-django.git cropcompass \
   > ~/logs/django-clone 2>&1
-mv cropcompass-django/cropcompass ~vagrant
 popd
 
-echo `date` "Making cropcompass virtualenv"
+echo `date` "Making and activating 'cropcompass' virtualenv"
 mkdir -p ~/Env
 virtualenv -p /usr/bin/python3 ~/Env/cropcompass > ~/logs/virtualenv 2>&1
+source ~/Env/cropcompass/bin/activate
 
 echo `date` "Installing Django requirements"
-source ~/Env/cropcompass/bin/activate
 pip install --upgrade pip > ~/logs/pip 2>&1
 pip install -r /vagrant/scripts/requirements.txt > ~/logs/requirements 2>&1
 
@@ -20,6 +19,5 @@ pushd ~/cropcompass
 echo `date` "Running migrations"
 python3 manage.py migrate > ~/logs/migrate 2>&1
 echo `date` "Collecting static assets"
-echo "STATIC_ROOT = os.path.join(BASE_DIR, \"static/\")" >> cropcompass/settings.py
 python3 manage.py collectstatic --noinput > ~/logs/static 2>&1
 popd
