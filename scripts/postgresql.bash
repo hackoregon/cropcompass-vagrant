49231b8a2782089faa2e51e8cdeb452c91b73c62
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-05-26 19:10:23 -0700

    grab SQL dump from GitHub

--- scripts/postgresql.bash
+++ scripts/postgresql.bash
@@ -6,6 +6,5 @@ createdb vagrant
 
 echo `date` "Restoring SQL dump"
 createuser alex
-rm -f agtechdump2016-05-19.sql
-wget --quiet https://www.dropbox.com/s/t6t1r3x6a4lp0zi/agtechdump2016-05-19.sql
-psql < /vagrant/agtech*sql > ~/logs/psql 2>&1
+curl -Ls https://github.com/hackoregon/cropcompass-sql-dumps/raw/master/agtechdump2016-05-19.sql \
+  | psql > ~/logs/psql 2>&1
