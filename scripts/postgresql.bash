c40846cf9f1048fedd90d5efa842b75a6ee93c6f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-03 06:13:21 +0000

    exit if already provisioned

--- scripts/postgresql.bash
+++ scripts/postgresql.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -e ~vagrant/.postgresql ]
+then
+  echo "PostgreSQL provisioning already done - exiting"
+  exit
+fi
+
 echo `date` "Creating the PostgreSQL 'vagrant' user and database"
 sudo su postgres -c "createuser -s vagrant"
 createdb vagrant
@@ -8,3 +14,5 @@ echo `date` "Restoring SQL dump"
 createuser alex
 curl -Ls https://github.com/hackoregon/cropcompass-sql-dumps/raw/master/agtechdump2016-05-19.sql \
   | psql > ~/logs/psql 2>&1
+
+touch ~vagrant/.postgresql
