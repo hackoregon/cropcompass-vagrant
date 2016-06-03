e47fca82471ca563d60d4026118cd90bf06fdd39
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-03 02:27:25 -0700

    activate virtualenv

--- scripts/django-app.bash
+++ scripts/django-app.bash
@@ -7,6 +7,7 @@ then
 fi
 
 echo `date` "Copying Django app from '/vagrant/django-app' to '~vagrant/cropcompass'"
+source ~/Env/cropcompass/bin/activate
 cp -rp /vagrant/django-app ~vagrant/cropcompass
 
 pushd ~/cropcompass
@@ -15,3 +16,7 @@ python3 manage.py migrate > ~/logs/migrate 2>&1
 echo `date` "Collecting static assets"
 python3 manage.py collectstatic --noinput > ~/logs/static 2>&1
 popd
+
+
+
+
