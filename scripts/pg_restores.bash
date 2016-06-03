cc1122699f18a2c3d5453d209658fd711aef09dc
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-03 06:16:36 +0000

    exit if already done

--- scripts/pg_restores.bash
+++ scripts/pg_restores.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ -e ~vagrant/.pg_restores ]
+then
+  echo "Secondary data already restored - exiting"
+  exit
+fi
 echo `date` "Restoring secondary data"
 curl -Ls https://raw.githubusercontent.com/hackoregon/cropcompass-sql-dumps/master/imports_dump \
   | psql > ~/logs/imports 2>&1
 curl -Ls https://raw.githubusercontent.com/hackoregon/cropcompass-sql-dumps/master/exports_dump \
   | psql > ~/logs/exports 2>&1
+
+touch ~vagrant/.pg_restores
