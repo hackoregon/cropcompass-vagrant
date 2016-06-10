15acad3fd83ee0a9292422c2fd458659aa72353d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-09 19:07:35 -0700

    get paths right

--- scripts/reload-django-code.bash
+++ scripts/reload-django-code.bash
@@ -3,6 +3,6 @@
 echo `date` "stopping app server"
 sudo service uwsgi stop
 echo `date` "Copying Django app from '/vagrant/django-app' to '~vagrant/cropcompass'"
-cp -rp /vagrant/django-app ~vagrant/cropcompass
+cp -rp /vagrant/django-app/* ~vagrant/cropcompass/
 echo `date` "starting app server"
 sudo service uwsgi start
