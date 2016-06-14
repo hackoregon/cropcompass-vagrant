49e8bd9def253f8402cc86a923ec5dbec442024e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-14 23:52:49 +0000

    fix loss of styling and static files

--- scripts/reload-django-code.bash
+++ scripts/reload-django-code.bash
@@ -3,8 +3,8 @@
 echo `date` "stopping app server"
 sudo service uwsgi stop
 echo `date` "backing up old cropcompass Django app"
-mv ~/cropcompass ~/cropcompass-`date -Iseconds`
+cp -rp ~/cropcompass ~/cropcompass-`date -Iseconds`
 echo `date` "copying Django app from '/vagrant/django-app' to '~vagrant/cropcompass'"
-cp -rp /vagrant/django-app ~vagrant/cropcompass
+cp -rp /vagrant/django-app/* ~vagrant/cropcompass/
 echo `date` "starting app server"
 sudo service uwsgi start
