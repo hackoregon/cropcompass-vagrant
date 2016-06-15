3780a688142b6109e708dba1a7a7d954f6a053ee
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-14 20:36:35 -0700

    issue #24

--- scripts/reload-django-code.bash
+++ scripts/reload-django-code.bash
@@ -3,8 +3,10 @@
 echo `date` "stopping app server"
 sudo service uwsgi stop
 echo `date` "backing up old cropcompass Django app"
-cp -rp ~/cropcompass ~/cropcompass-`date -Iseconds`
+export STAMP=`date -Iseconds`
+cp -rp ~/cropcompass ~/cropcompass-${STAMP}
 echo `date` "copying Django app from '/vagrant/django-app' to '~vagrant/cropcompass'"
+diff -Naur /vagrant/django-app ~vagrant/cropcompass > ~vagrant/logs/${STAMP}.diff
 cp -rp /vagrant/django-app/* ~vagrant/cropcompass/
 echo `date` "starting app server"
 sudo service uwsgi start
