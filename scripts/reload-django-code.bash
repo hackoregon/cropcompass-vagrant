15e7583fa61c259afa163e4674bd66931f698300
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-15 03:58:50 +0000

    quieter diff

--- scripts/reload-django-code.bash
+++ scripts/reload-django-code.bash
@@ -6,7 +6,7 @@ echo `date` "backing up old cropcompass Django app"
 export STAMP=`date -Iseconds`
 cp -rp ~/cropcompass ~/cropcompass-${STAMP}
 echo `date` "copying Django app from '/vagrant/django-app' to '~vagrant/cropcompass'"
-diff -Naur /vagrant/django-app ~vagrant/cropcompass > ~vagrant/logs/${STAMP}.diff
+diff -r /vagrant/django-app ~vagrant/cropcompass > ~vagrant/logs/${STAMP}.diff
 cp -rp /vagrant/django-app/* ~vagrant/cropcompass/
 echo `date` "starting app server"
 sudo service uwsgi start
