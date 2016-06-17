60198fda007c813c9272828022255072cc6e7e54
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-17 06:08:35 +0000

    stop and restart app server

--- scripts/cleaned-export-data.bash
+++ scripts/cleaned-export-data.bash
@@ -6,6 +6,9 @@ then
   exit
 fi
 
+echo `date` "stopping app server"
+sudo service uwsgi stop
+
 echo `date` "Restoring cleaned export data SQL dump"
 curl -Ls \
   https://github.com/hackoregon/cropcompass-sql-dumps/raw/master/exports_historical_cleaned2016_06_16.sql \
@@ -13,5 +16,8 @@ curl -Ls \
 
 echo `date` "Logging table details to ~vagrant/logs/tables-cleaned"
 psql -c '\d+' > ~vagrant/logs/tables-cleaned
+
+echo `date` "starting app server"
+sudo service uwsgi start
 
 touch ~vagrant/.cleaned-export-data
