03327c128e0bac8e927512dc8e494cb666a74d95
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-16 23:00:57 -0700

    add cleaned export data

---
+++ scripts/cleaned-export-data.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -e ~vagrant/.cleaned-export-data ]
+then
+  echo `date` "Cleaned export data already loaded - exiting"
+  exit
+fi
+
+echo `date` "Restoring cleaned export data SQL dump"
+curl -Ls \
+  https://github.com/hackoregon/cropcompass-sql-dumps/raw/master/exports_historical_cleaned2016_06_16.sql \
+  | psql > ~/logs/psql-cleaned 2>&1
+
+echo `date` "Logging table details to ~vagrant/logs/tables-cleaned"
+psql -c '\d+' > ~vagrant/logs/tables-cleaned
+
+touch ~vagrant/.cleaned-export-data
